c2d7e2ca9f2ce428b9ed8211faf6e9880d2e2fc3
papachi-one <pavel.csefalvay@papachi.one>
2022-11-07 21:43:03 +0100

    code update - work in progress

--- src/main/java/one/papachi/httpd/api/http/HttpClient.java
+++ src/main/java/one/papachi/httpd/api/http/HttpClient.java
@@ -1,9 +1,19 @@
 package one.papachi.httpd.api.http;
 
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 
 public interface HttpClient {
 
-    CompletableFuture<HttpResponse> send(HttpRequest request);
+    CompletableFuture<HttpResponse> send(String host, int port, boolean https, HttpRequest request);
+
+    ExecutorService getExecutorService();
+
+    Set<HttpOption<?>> supportedOptions();
+
+    <T> T getOption(HttpOption<T> name);
+
+    <T> HttpClient setOption(HttpOption<T> name, T value);
 
 }
